5a33280187b72d6214e937fd39ba308147052879
Daniel Thurau <daniel.thurau@dfinity.org>
2022-05-06 12:20:11 -0700

    Cargo fmt error fixed

--- src/main.rs
+++ src/main.rs
@@ -123,10 +123,12 @@ fn parse_canister_id(
     key_name: &str,
     canister_id_map: &HashMap<String, String>,
 ) -> AnyhowResult<CanisterId> {
-    let value = canister_id_map.get(key_name).ok_or_else(|| anyhow!(
-        "'{}' is not present in --canister-ids-file <file>",
-        key_name
-    ))?;
+    let value = canister_id_map.get(key_name).ok_or_else(|| {
+        anyhow!(
+            "'{}' is not present in --canister-ids-file <file>",
+            key_name
+        )
+    })?;
     let canister_id = CanisterId::from_str(value)
         .map_err(|err| anyhow!("Could not parse CanisterId of '{}': {}", key_name, err))?;
     Ok(canister_id)
