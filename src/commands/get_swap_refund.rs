453c0e4763a6496791a11f76e4bf9c5328b820a6
Daniel Thurau <daniel.thurau@dfinity.org>
2023-01-04 12:10:53 -0800

    Bump the version of the IC repo to handle the new ManageNeuronResponse variant (#29)
    * Bump the version of the IC repo to handle the new ManageNeuronResponse variant
    
    * Fix fmt issue and bump version to 0.4.1

--- src/commands/get_swap_refund.rs
+++ src/commands/get_swap_refund.rs
@@ -1,5 +1,6 @@
 use candid::Encode;
 use clap::Parser;
+use ic_base_types::PrincipalId;
 use ic_sns_swap::pb::v1::ErrorRefundIcpRequest;
 
 use crate::{
@@ -10,19 +11,15 @@ use crate::{
     SnsCanisterIds,
 };
 
-use super::transfer;
-
 /// Signs a message to request a refund from the SNS swap canister.
 /// If the swap was aborted or failed, or some of your contributed ICP never made it into a neuron,
 /// this command can retrieve your unused ICP, minus transaction fees.
 #[derive(Parser)]
 pub struct GetSwapRefundOpts {
-    /// The amount of ICP to request a refund for.
+    /// The principal that made the ICP contribution and should be refunded. The ICP will be
+    /// refunded to the main account of this Principal irrespective of the caller.
     #[clap(long)]
-    amount: String,
-    /// The expected transaction fee. If omitted, defaults to 0.0001 ICP.
-    #[clap(long)]
-    fee: Option<String>,
+    principal: PrincipalId,
 }
 
 pub fn exec(
@@ -30,15 +27,8 @@ pub fn exec(
     sns_canister_ids: &SnsCanisterIds,
     opts: GetSwapRefundOpts,
 ) -> AnyhowResult<Vec<IngressWithRequestId>> {
-    let tokens = transfer::parse_tokens(&opts.amount)?.get_e8s();
-    let fee = opts
-        .fee
-        .map(|fee| anyhow::Ok(transfer::parse_tokens(&fee)?.get_e8s()))
-        .transpose()?
-        .unwrap_or(10_000);
     let message = ErrorRefundIcpRequest {
-        icp_e8s: tokens,
-        fee_override_e8s: fee,
+        source_principal_id: Some(opts.principal),
     };
     let req = sign_ingress_with_request_status_query(
         pem,
--- tests/commands/refund.sh
+++ tests/commands/refund.sh
@@ -1,1 +1,1 @@
-${CARGO_TARGET_DIR:-../target}/debug/sns-quill --canister-ids-file ./canister_ids.json --pem-file - get-swap-refund --amount 500 | ${CARGO_TARGET_DIR:-../target}/debug/sns-quill send --dry-run -
+${CARGO_TARGET_DIR:-../target}/debug/sns-quill --canister-ids-file ./canister_ids.json --pem-file - get-swap-refund --principal fdsgv-62ihb-nbiqv-xgic5-iefsv-3cscz-tmbzv-63qd5-vh43v-dqfrt-pae | ${CARGO_TARGET_DIR:-../target}/debug/sns-quill send --dry-run -
